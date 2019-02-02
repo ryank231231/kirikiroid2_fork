51a6dbb07afd9e0a01d1a76a317c44e5e62f250f
weimingtom <weimingtom@qq.com>
2019-02-02 12:19:42 +0800

    mod

--- build/clean.bat
+++ build/clean.bat
@@ -4,6 +4,8 @@
 @rmdir /S /Q ..\cocos\2d\Debug.win32
 @rmdir /S /Q ..\external\zlib-1.2.5\Debug
 @rmdir /S /Q ..\external\libpng-1.4.5\Debug
+@rmdir /S /Q ..\tests\cpp-empty-test\Classes\core\external\opencv-2.4.13\modules\core\Debug
+@rmdir /S /Q ..\tests\cpp-empty-test\Classes\core\external\opencv-2.4.13\modules\imgproc\Debug
 @rmdir /S /Q Debug.win32
 @MSBuild cocos2d-win32.vc2010.sln /t:Clean /p:Configuration=Debug /p:Platform=win32
 @pause
